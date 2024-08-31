3f734497d4f9ce23b1dfdc3a2552b02715ba4304
Cris <cristiansouza.agra@gmail.com>
2024-08-31 04:16:25 -0300

    feat: Criação da Service de Empresas

--- src/services/empresasService.ts
+++ src/services/empresasService.ts
@@ -1,32 +1,32 @@
 import axios from "axios";
-import { Parceiro } from "../types/Parceiro";
+import { Empresa } from "../types/Empresa";
 
 export const api = axios.create({
-  baseURL: "https://644060ba792fe886a88de1b9.mockapi.io",
+  baseURL: "https://655cf25525b76d9884fe3153.mockapi.io/v1",
 });
 
-export const fetchParceiros = async () => {
-  const { data } = await api.get("/v1/test/partners");
+export const fetchEmpresas = async () => {
+  const { data } = await api.get("/external-companies");
 
   return data;
 };
 
-export const createParceiro = async (parceiro: Parceiro) => {
-  const { data } = await api.post("/v1/test/partners", parceiro);
+export const createEmpresa = async (empresa: Empresa) => {
+  const { data } = await api.post("/external-companies", empresa);
   return data;
 };
 
-export const updateParceiro = async (id: string, parceiro: Partial<Parceiro>) => {
-  const { data } = await api.put(`/v1/test/partners/${id}`, parceiro);
+export const updateEmpresa = async (id: string, empresa: Partial<Empresa>) => {
+  const { data } = await api.put(`/external-companies/${id}`, empresa);
   return data;
 };
 
-export const fetchParceiro = async (id: string) => {
-  const { data } = await api.get(`/v1/test/partners/${id}`);
+export const fetchEmpresa = async (id: string) => {
+  const { data } = await api.get(`/external-companies/${id}`);
   return data;
 };
 
-export const deleteParceiro = async (id: string) => {
-  const { data } = await api.delete(`/v1/test/partners/${id}`);
+export const deleteEmpresa = async (id: string) => {
+  const { data } = await api.delete(`/external-companies/${id}`);
   return data;
 };
---
+++ src/types/Empresa.ts
@@ -0,0 +1,9 @@
+export type Empresa = {
+  createdAt: string;
+  companyName: string;
+  collaboratorsCount: number;
+  isActive: boolean;
+  lastSubmit: string;
+  id: string;
+  name: string;
+};
